b6273bcdda7d3d1f89247e479c5765d2b33e3fb6
peachkoder <peachkoder@gmail.org>
2022-10-15 16:11:06 +0100

    ooops initial state on repositories reducers

---
+++ src/componets/RepositoriesList.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+type Props = {};
+
+const RepositoriesList: React.FC = () => {
+  return (
+    <div>
+      <form action="">
+        <input type="text" />
+        <button>Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default RepositoriesList;
